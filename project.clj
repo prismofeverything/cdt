0a358ec07a72199d46d79b2632317c90531097b4
ryan spangler <ryan.spangler@gmail.com>
2013-11-22 11:43:33 -0800

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismofeverything/cdt  "1.2.6.3"
+(defproject prismofeverything/cdt  "1.2.6.4"
   :description "Clojure Debugging Toolkit"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.gjahad/debug-repl "0.3.1"]])
